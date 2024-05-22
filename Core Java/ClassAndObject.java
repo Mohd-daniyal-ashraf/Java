33d55376d5e543752a98106bb287c834483000b8
mohd daniyal ashraf <daniyalashraf907@gmail.com>
2024-05-22 18:23:35 +0530

    save

--- Core Java/ClassAndObject.java
+++ Core Java/ClassAndObject.java
@@ -5,17 +5,18 @@ class Carinfo{
     void breamaro(String action){
         System.out.println(action);
     }
+    Carinfo(String name,String color,int price){
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
 }
 
 class ClassAndObject{
     public static void main(String[] args){
-       Carinfo c= new Carinfo();
-       c.name = "Bugati";
-       c.color="Black";
-       c.price = 100000000;
+       Carinfo c= new Carinfo("Bugati","Black",10000);
        System.err.println(c.name);
        System.err.println(c.color);
        System.err.println(c.price);
-       c.breamaro("Ha bhai kya haal hai");
     }
 }
