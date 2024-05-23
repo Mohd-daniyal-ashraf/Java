f06745d741ef0350dd738c8ec15ab7851a343622
mohd daniyal ashraf <daniyalashraf907@gmail.com>
2024-05-23 18:36:53 +0530

    save

--- Core Java/array.java
+++ Core Java/array.java
@@ -4,12 +4,17 @@ public class array {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[][] arr = new int[n][n];
         for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
+            for(int j=0;j<n;j++){
+                arr[i][j] = sc.nextInt();
+            }
         }
         for(int i=0;i<n;i++){
-            System.out.println(arr[i]);
+            for(int j=0;j<n;j++){
+                System.out.println(arr[i][j] +" ");
+            }
+            System.out.println();
         }
     }
 }
---
+++ Core Java/consitional_flow.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class consitional_flow {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n == -1){
+            System.out.println("good");
+        }
+        else{
+            System.out.println("Not good");
+        }
+    }
+}
