945377cf3a3b9854e8f37fe065f94c6ed8f72bd3
mohd daniyal ashraf <daniyalashraf907@gmail.com>
2024-06-18 06:26:50 +0530

    new tech

--- method.java
+++ method.java
@@ -1,14 +1,33 @@
-// package com.methods.demo;
 public class method {
     public static void main(String[] args){
         squredemo sd = new squredemo();
-        sd.findsq();
+        // sd.findsq(4);
+        int ans = sd.findsq(5);
+        System.out.println(ans);
     }   
 }
+
+//Method - not return - no arguments
+// class squredemo{
+//     void findsq(){
+//         int a = 20;
+//         a = a * a;
+//         System.out.println("square "+a);
+//     }
+// }
+
+//Method - not return - with arguments
+// class squredemo{
+//     void findsq(int a){
+//         a = a * a;
+//         System.out.println("square "+a);
+//     }
+// }
+
+//Method -  return - with arguments
 class squredemo{
-    void findsq(){
-        int a = 20;
+    int findsq(int a){
         a = a * a;
-        System.out.println("square "+a);
+        return a;
     }
 }
